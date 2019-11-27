2d928d135ada2fb46560a62a2262fce196e0d0fe
flobos <fernandolobos@gmail.com>
2019-11-27 16:03:37 -0300

    Avd Cost, unit price

--- models/stock_move_line_inherit.py
+++ models/stock_move_line_inherit.py
@@ -10,6 +10,50 @@ class stock_picking_reports(models.Model):
     total_unidades_salida = fields.Float(string="Unidades Salida", readonly=True, store=True, compute='_calcular_total_unidades_salida')
     total_unidades = fields.Float(string="Total Unidades", readonly=True, store=True,
                                           compute='_calcular_total_unidades_totales')
+    precio_unidad_compra = fields.Float(string="Precio U. Compra", readonly=True, store=True,
+                                          compute='_calcular_precio_unidad_compra')
+
+    valor_costo_promedio = fields.Float(string="Costo", readonly=True, store=True,
+                                          compute='_calcular_precio_costo')
+    valor_debe = fields.Float(string="Debe", readonly=True, store=True,
+                                          compute='_calcular_valor_debe')
+
+    @api.multi
+    @api.depends('precio_unidad_compra', 'total_unidades_entrada')
+    def _calcular_valor_debe(self):
+        for rec in self:
+            if rec.move_id.purchase_line_id.id != False:
+                rec.valor_debe = rec.total_unidades_entrada * rec.precio_unidad_compra
+
+
+    @api.multi
+    @api.depends('qty_done')
+    def _calcular_precio_costo(self):
+        stock_line = self.env['stock.move.line']
+        for rec in self:
+            contador = 0
+            total = 0
+            stock_ids = stock_line.search([('product_id', '=', rec.product_id.id)])
+            if stock_ids:
+                for stock in stock_ids:
+                    if stock.move_id.purchase_line_id.id != False:
+                        total += stock.move_id.purchase_line_id.price_unit
+                        contador += 1
+        if  total != 0 and  contador != 0:
+            rec.valor_costo_promedio = total / contador
+
+
+
+
+    @api.multi
+    @api.depends('qty_done')
+    def _calcular_precio_unidad_compra(self):
+        for rec in self:
+            if rec.move_id.purchase_line_id.id != False:
+                rec.precio_unidad_compra = rec.move_id.purchase_line_id.price_unit
+            else:
+                rec.precio_unidad_compra = 0
+
 
     @api.multi
     @api.depends('qty_done')
